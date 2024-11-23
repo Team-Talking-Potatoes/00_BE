4c1bb166263ad77774d68245d55e8262e0a70a05
jickDo <pocard0117@naver.com>
2024-11-23 22:10:36 +0900

    [refactor]: 기존 복합 id를 기본 id를 사용하기로 함 & 중간테이블 객체의 readOnly를 제거

--- src/main/java/potatoes/server/entity/UserGathering.java
+++ src/main/java/potatoes/server/entity/UserGathering.java
@@ -3,9 +3,11 @@ package potatoes.server.entity;
 import java.time.Instant;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
@@ -16,17 +18,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserGathering {
 
-	@EmbeddedId
-	private UserGatheringId userGatheringId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	@Column(name = "joined_at", nullable = false)
 	private Instant joinedAt;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id", insertable = false, updatable = false)
+	@JoinColumn(name = "user_id")
 	private User user;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "gathering_id", insertable = false, updatable = false)
+	@JoinColumn(name = "gathering_id")
 	private Gathering gathering;
 }
