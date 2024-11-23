969beff8d276fda086d119d4501c8afe110e03b5
jickDo <pocard0117@naver.com>
2024-11-23 13:50:08 +0900

    feat: 유저 엔티티 생성

---
+++ src/main/java/potatoes/server/entity/User.java
@@ -0,0 +1,53 @@
+package potatoes.server.entity;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import potatoes.server.config.BaseTimeEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id", nullable = false)
+	private Long id;
+
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "company_name", nullable = false)
+	private String companyName;
+
+	@Column(name = "image")
+	private String image;
+
+	@Column(name = "deleted_at")
+	private Instant deletedAt;
+
+	@OneToMany(mappedBy = "user")
+	private List<Gathering> gatherings = new ArrayList<>();
+
+	@OneToMany(mappedBy = "user")
+	private List<UserGathering> joined = new ArrayList<>();
+
+	@OneToMany(mappedBy = "user")
+	private List<Review> reviews = new ArrayList<>();
+}
