8352ba8015706c1736fa6d254e3fd825c829a125
Lim Jick Chan <95222741+jickDo@users.noreply.github.com>
2024-11-24 17:14:54 +0900

    ✨feature: JWT 관련 기능 생성 (#9)
    Co-authored-by: Dev_its <itsmine096@gmail.com>

---
+++ src/main/java/potatoes/server/utils/annotation/AuthorizationConfig.java
@@ -0,0 +1,21 @@
+package potatoes.server.utils.annotation;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class AuthorizationConfig implements WebMvcConfigurer {
+
+	private final AuthorizationArgumentResolver authorizationArgumentResolver;
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(authorizationArgumentResolver);
+	}
+}
