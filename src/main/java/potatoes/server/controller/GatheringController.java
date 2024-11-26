319d520f9966ea899f92f486788d604071891455
jickDo <pocard0117@naver.com>
2024-11-27 00:14:16 +0900

    [feat]: 모임 목록 조회 api

--- src/main/java/potatoes/server/controller/GatheringController.java
+++ src/main/java/potatoes/server/controller/GatheringController.java
@@ -1,19 +1,24 @@
 package potatoes.server.controller;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import potatoes.server.constant.GatheringType;
+import potatoes.server.constant.SortByType;
 import potatoes.server.dto.CreateGatheringRequest;
 import potatoes.server.dto.CreateGatheringResponse;
 import potatoes.server.dto.GetGatheringRequest;
@@ -30,28 +35,38 @@ public class GatheringController {
 	private final GatheringService gatheringService;
 	private final PageableFactory pageableFactory;
 
-	// @GetMapping("/")
-	// public Page<GetGatheringResponse> getGathering(
-	// 	@RequestBody GetGatheringRequest req
-	// ) {
-	// 	return gatheringService.getGatheringResponseList(req);
-	// }
+	private static final String DEFAULT_SORT_BY = "createdAt";
+	private static final String DEFAULT_SORT_ORDER = "desc";
 
 	@GetMapping("/")
-	public List<GetGatheringResponse> getGatherings(
-		@RequestBody GetGatheringRequest req
-	) {
-		Pageable pageable = pageableFactory.create(req);
-		return gatheringService.getGatherings(req, pageable);
+	public List<GetGatheringResponse> getGatherings(@RequestParam(required = false) String id,
+		@RequestParam(required = false) GatheringType type, @RequestParam(required = false) String location,
+		@RequestParam(required = false) String date, @RequestParam(required = false) Long createdBy,
+		@RequestParam(required = false, defaultValue = DEFAULT_SORT_BY) SortByType sortBy,
+		@RequestParam(required = false, defaultValue = DEFAULT_SORT_ORDER) String sortOrder,
+		@RequestParam(required = false, defaultValue = "20") @Valid @Min(value = 1, message = "Limit의 최소값은 1입니다.") int limit,
+		@RequestParam(required = false, defaultValue = "0") @Valid @Min(value = 0, message = "offset의 최소값은 0입니다.") int offset) {
+		List<Long> idList = id != null ?
+			Arrays.stream(id.split(",")).map(String::trim).map(Long::parseLong).collect(Collectors.toList()) : null;
+
+		GetGatheringRequest request = GetGatheringRequest.builder()
+			.ids(idList)
+			.type(type)
+			.location(location)
+			.date(date)
+			.createdBy(createdBy)
+			.sortBy(sortBy)
+			.sortOrder(sortOrder)
+			.limit(limit)
+			.offset(offset)
+			.build();
+
+		Pageable pageable = pageableFactory.create(request);
+		return gatheringService.getGatherings(request, pageable);
 	}
-	
-	@PostMapping(
-		value = "/",
-		consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
-		produces = MediaType.APPLICATION_JSON_VALUE
-	)
-	public CreateGatheringResponse createGathering(
-		@Authorization Long memberId,
+
+	@PostMapping(value = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public CreateGatheringResponse createGathering(@Authorization Long memberId,
 		@RequestPart("gatheringInfo") @Valid CreateGatheringRequest request,
 		@RequestPart(value = "image", required = false) MultipartFile multipartFile) {
 		return gatheringService.integrateGatheringCreation(request, multipartFile, memberId);
--- src/main/java/potatoes/server/dto/GetGatheringRequest.java
+++ src/main/java/potatoes/server/dto/GetGatheringRequest.java
@@ -1,34 +1,28 @@
 package potatoes.server.dto;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.ZoneOffset;
-
-import javax.swing.*;
-
-import org.springframework.format.annotation.DateTimeFormat;
+import java.util.List;
 
 import jakarta.validation.constraints.Min;
+import lombok.Builder;
 import potatoes.server.constant.GatheringType;
 import potatoes.server.constant.SortByType;
 import potatoes.server.utils.Pagination.Paginator;
 
+@Builder
 public record GetGatheringRequest(
-	String id,
+	List<Long> ids,
 
 	GatheringType type,
 
 	String location,
 
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	LocalDate date,
+	String date,
 
 	Long createdBy,
 
 	SortByType sortBy,
 
-	SortOrder sortOrder,
+	String sortOrder,
 
 	@Min(value = 1, message = "Limit의 최소값은 1입니다.") int limit,
 
@@ -55,16 +49,4 @@ public record GetGatheringRequest(
 		return sortOrder != null ? sortOrder.name() : "desc";
 	}
 
-	public Instant getStartOfDay() {
-		return date != null
-			? date.atStartOfDay(ZoneOffset.UTC).toInstant()
-			: null;
-	}
-
-	// 해당 날짜의 끝 시간 (2024-07-06 23:59:59.999999999 UTC)
-	public Instant getEndOfDay() {
-		return date != null
-			? date.atTime(LocalTime.MAX).toInstant(ZoneOffset.UTC)
-			: null;
-	}
 }
