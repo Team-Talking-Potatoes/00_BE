b7749d33ded981f62b1279ec6bcfac37c943ec2e
Hyun <itsmine096@gmail.com>
2024-11-27 16:32:22 +0900

    [chore] 오타 수정, ErrorCode 분류 주석 추가

--- src/main/java/potatoes/server/error/ErrorCode.java
+++ src/main/java/potatoes/server/error/ErrorCode.java
@@ -4,10 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-	INVALID_INPUT_VALUE(400, "INVALID_INPUT_VALUE", " Invalid Input Value"),
+	// Common
 	INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "Server Error"),
-	INVALID_TYPE_VALUE(400, "INVALID_TYPE_VALUE", " Invalid Type Value");
-	
+	INVALID_INPUT_VALUE(400, "INVALID_INPUT_VALUE", "Invalid Input Value"),
+	INVALID_TYPE_VALUE(400, "INVALID_TYPE_VALUE", "Invalid Type Value");
+
 	private final int status;
 	private final String code;
 	private final String message;
