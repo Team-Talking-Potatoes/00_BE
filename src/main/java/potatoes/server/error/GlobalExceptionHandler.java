97fd958245d0235d1bd582b48fffc61f826e132e
Hyun <itsmine096@gmail.com>
2024-11-27 16:35:01 +0900

    [refactor] ErrorCode static input 하도록 변경

--- src/main/java/potatoes/server/error/GlobalExceptionHandler.java
+++ src/main/java/potatoes/server/error/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package potatoes.server.error;
 
+import static potatoes.server.error.ErrorCode.*;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,7 +15,7 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-		ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+		ErrorCode errorCode = INVALID_INPUT_VALUE;
 		ErrorResponse response = ErrorResponse.of(errorCode, e.getBindingResult());
 		return ResponseEntity.status(errorCode.getStatus()).body(response);
 	}
@@ -22,20 +24,19 @@ public class GlobalExceptionHandler {
 	protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
 		MethodArgumentTypeMismatchException e) {
 		ErrorResponse response = ErrorResponse.of(e);
-		return ResponseEntity.status(400).body(response);
+		return ResponseEntity.status(INVALID_TYPE_VALUE.getStatus()).body(response);
 	}
 
 	@ExceptionHandler(CustomException.class)
 	protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
 		ErrorCode errorCode = e.getErrorCode();
-		ErrorResponse response = ErrorResponse.of(errorCode);
+		ErrorResponse response = ErrorResponse.from(errorCode);
 		return ResponseEntity.status(errorCode.getStatus()).body(response);
 	}
 
 	@ExceptionHandler(Exception.class)
 	protected ResponseEntity<ErrorResponse> handleException(Exception e) {
-		ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
-		ErrorResponse response = ErrorResponse.of(errorCode);
-		return ResponseEntity.status(errorCode.getStatus()).body(response);
+		ErrorResponse response = ErrorResponse.from(INTERNAL_SERVER_ERROR);
+		return ResponseEntity.status(INTERNAL_SERVER_ERROR.getStatus()).body(response);
 	}
 }
