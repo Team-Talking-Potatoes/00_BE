7bf23a9ca937f72bcf5b6852530ce0877d64951f
Hyun <itsmine096@gmail.com>
2024-11-27 21:55:23 +0900

    [chore] 기존 브랜치에서 생성한 예외 처리로직 제거

--- src/main/java/potatoes/server/exception/AlreadyExistsEmailException.java
+++
@@ -1,8 +0,0 @@
-package potatoes.server.exception;
-
-public class AlreadyExistsEmailException extends CustomException {
-
-	public AlreadyExistsEmailException() {
-		super(ErrorCode.EMAIL_DUPLICATION);
-	}
-}
