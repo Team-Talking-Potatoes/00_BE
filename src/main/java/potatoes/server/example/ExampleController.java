6d4beeb648b642751720df9b7bfa9bd64250f600
jickDo <pocard0117@naver.com>
2024-12-04 17:14:48 +0900

    [feat] 예제 s3 파일 등록

--- src/main/java/potatoes/server/example/ExampleController.java
+++ src/main/java/potatoes/server/example/ExampleController.java
@@ -1,10 +1,16 @@
 package potatoes.server.example;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,6 +19,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import potatoes.server.utils.annotation.Authorization;
 import potatoes.server.utils.jwt.JwtTokenUtil;
+import potatoes.server.utils.s3.S3UtilsProvider;
 
 @Tag(name = "예제용 보일러플레이트")
 @RequiredArgsConstructor
@@ -20,6 +27,39 @@ import potatoes.server.utils.jwt.JwtTokenUtil;
 public class ExampleController {
 
 	private final JwtTokenUtil jwtTokenUtil;
+	private final S3UtilsProvider s3UtilsProvider;
+
+	@Operation(summary = "s3 멀티파트 등록 예제 테스트")
+	@PostMapping(
+		value = "/test/file",
+		consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+		produces = MediaType.APPLICATION_JSON_VALUE
+	)
+	public ResponseEntity<String> postToS3(
+		@RequestPart("multipartFile") MultipartFile multipartFile
+	) {
+		return ResponseEntity.ok(s3UtilsProvider.uploadFile(multipartFile));
+	}
+
+	@Operation(summary = "s3 멀티파트 다중 등록 예제 테스트")
+	@PostMapping(
+		value = "/test/files",
+		consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+		produces = MediaType.APPLICATION_JSON_VALUE
+	)
+	public ResponseEntity<List<String>> postToS3(
+		@RequestPart("multipartFiles") List<MultipartFile> multipartFiles
+	) {
+		return ResponseEntity.ok(s3UtilsProvider.uploadFiles(multipartFiles));
+	}
+
+	@Operation(summary = "s3 파일이름으로 주소 찾기")
+	@PostMapping("/test/file/url")
+	public ResponseEntity<String> generateUrl(
+		@RequestParam String filename
+	) {
+		return ResponseEntity.ok(s3UtilsProvider.getFileUrl(filename));
+	}
 
 	@Operation(summary = "토큰 발급")
 	@GetMapping("/token")
