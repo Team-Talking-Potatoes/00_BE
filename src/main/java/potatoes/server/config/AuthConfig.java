e18906b26841d0dc92e93156d25e1382d69226f5
jickDo <pocard0117@naver.com>
2024-12-07 15:47:39 +0900

    [chore] swagger 타겟위치 단독으로 잡기

--- src/main/java/potatoes/server/config/AuthConfig.java
+++ src/main/java/potatoes/server/config/AuthConfig.java
@@ -4,14 +4,16 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import potatoes.server.controller.UserController;
+
 @Configuration
 public class AuthConfig {
 	@Bean
 	public GroupedOpenApi getAuthApi() {
 		return GroupedOpenApi.builder()
 			.group("auth")
-			.pathsToMatch("/controller/UserController")
-			.pathsToExclude("")
+			.addOpenApiMethodFilter(method ->
+				method.getDeclaringClass().equals(UserController.class))
 			.packagesToScan("potatoes.server.controller")
 			.build();
 	}
