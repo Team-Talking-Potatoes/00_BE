ce83a3ee64f8449566bf4b0787cce1f0ddbb6938
jickDo <pocard0117@naver.com>
2024-12-09 16:22:52 +0900

    [feat] 메일 전송긴능만 하는 것으로 이름 변경 및 메일 추가 및 역할 분리

---
+++ src/main/java/potatoes/server/service/MailVerificationService.java
@@ -0,0 +1,68 @@
+package potatoes.server.service;
+
+import java.time.Duration;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import potatoes.server.dto.AccessToken;
+import potatoes.server.dto.SendMailRequest;
+import potatoes.server.error.exception.MailVerifyNumberExpired;
+import potatoes.server.error.exception.MailVerifyNumberNotValid;
+import potatoes.server.utils.GenerateRandomNumber;
+import potatoes.server.utils.jwt.JwtTokenUtil;
+import potatoes.server.utils.redis.RedisVerificationStore;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class MailVerificationService {
+
+	private final MailSender mailSender;
+	private final UserService userService;
+	private final RedisVerificationStore redisStore;
+	private final JwtTokenUtil jwtTokenUtil;
+	private static final Duration EXPIRATION = Duration.ofMinutes(5);
+
+	@Transactional
+	public void sendSignupEmail(SendMailRequest request) {
+		userService.validateEmailNotExists(request.email());
+		String verifyNumber = createAndStoreVerificationNumber(request.email());
+		mailSender.sendVerificationMail(request, verifyNumber);
+	}
+
+	@Transactional
+	public void sendPasswordResetEmail(SendMailRequest request) {
+		userService.validateEmailExists(request.email());
+		String verifyNumber = createAndStoreVerificationNumber(request.email());
+		mailSender.sendVerificationMail(request, verifyNumber);
+	}
+
+	public AccessToken verifyNumberAndCreateToken(String verifyNumber, String email) {
+		verifyNumberByEmail(verifyNumber, email);
+		return createAccessToken();
+	}
+
+	private String createAndStoreVerificationNumber(String email) {
+		String verifyNumber = GenerateRandomNumber.generateNumber();
+		redisStore.store(email, verifyNumber, EXPIRATION);
+		return verifyNumber;
+	}
+
+	private void verifyNumberByEmail(String verifyNumber, String email) {
+		String storedNumber = redisStore.find(email);
+		if (storedNumber == null) {
+			throw new MailVerifyNumberExpired();
+		}
+
+		if (!storedNumber.equals(verifyNumber)) {
+			throw new MailVerifyNumberNotValid();
+		}
+		redisStore.remove(email);
+	}
+
+	private AccessToken createAccessToken() {
+		return new AccessToken(jwtTokenUtil.createToken("verification"));
+	}
+}
