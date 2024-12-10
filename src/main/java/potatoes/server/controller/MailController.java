ba0da775f5ed18c62d2d6701a0865f2ce3835ab2
jickDo <pocard0117@naver.com>
2024-12-10 09:29:58 +0900

    [feat] 비밀번호 찾기의 경우 헤더에 토큰추가하게 변경

--- src/main/java/potatoes/server/controller/MailController.java
+++ src/main/java/potatoes/server/controller/MailController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import potatoes.server.dto.EmailVerifyRequest;
 import potatoes.server.dto.SendMailRequest;
 import potatoes.server.dto.VerifyResponse;
 import potatoes.server.service.MailVerificationService;
+import potatoes.server.utils.annotation.Authorization;
 
 @Tag(name = "Mail", description = "Mail API")
 @RequestMapping("/auth")
@@ -32,9 +34,19 @@ public class MailController {
 		return ResponseEntity.ok().build();
 	}
 
+	/***
+	 *
+	 * TODO 개선점으로 인가를 진행할때 유저의 id를 필요로 하지 않는 경우에 대한 메서드 상단 위치에 올수있는 인가 어노테이션을 추가하고 반환을 받지 않는 방식을
+	 *  구현해보면 어떨가 생각합니다. 지금처럼 인터셉터나 혹은 AOP를 사용해봐도 재밌을것 같습니다.
+	 *
+	 */
+
 	@Operation(summary = "메일 전송(비밀번호찾기)", description = "비밀번호용 메일 전송, 이메일이 존재하지 않으면 예외터트림")
 	@PostMapping("/password/emails")
-	public ResponseEntity<Void> validateEmailExistsForPasswordReset(@RequestBody @Valid SendMailRequest request) {
+	public ResponseEntity<Void> validateEmailExistsForPasswordReset(
+		@RequestBody @Valid SendMailRequest request,
+		@Authorization @Parameter(hidden = true) Long userid
+	) {
 		mailVerificationService.sendPasswordResetEmail(request);
 		return ResponseEntity.ok().build();
 	}
@@ -44,7 +56,8 @@ public class MailController {
 	public ResponseEntity<VerifyResponse> verifyNumber(
 		@RequestBody @Valid EmailVerifyRequest request
 	) {
-		VerifyResponse verifyResponse = mailVerificationService.verifyNumberAndCreateToken(request.verifyNumber(), request.email());
+		VerifyResponse verifyResponse = mailVerificationService.verifyNumberAndCreateToken(request.verifyNumber(),
+			request.email());
 		return ResponseEntity.ok().body(verifyResponse);
 	}
 }
