72c9b9ea4d3652b8ce7430ca166fd152a4480e5f
jickDo <pocard0117@naver.com>
2024-12-11 14:05:06 +0900

    [refactor] 새로운 비밀번호와 기존 비밀번호를 valid단에서 알려주게 변경

--- src/main/java/potatoes/server/dto/UnauthorizedPasswordResetRequest.java
+++ src/main/java/potatoes/server/dto/UnauthorizedPasswordResetRequest.java
@@ -9,7 +9,7 @@ public record UnauthorizedPasswordResetRequest(
 	@NotBlank(message = "이메일은 필수입니다")
 	String email,
 
-	@NotBlank(message = "비밀번호는 필수입니다")
+	@NotBlank(message = "새로운 비밀번호는 필수입니다")
 	@Pattern(
 		regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
 		message = "비밀번호는 8~20자리이며, 영문/숫자/특수문자를 모두 포함해야 합니다."
