865430a7a953079829e17053f5301514b5fb2467
jickDo <pocard0117@naver.com>
2024-12-22 19:01:48 +0900

    [refactor] 토큰 유효시간 임시로 1y, 및 https 설정 제거

--- src/main/java/potatoes/server/utils/crypto/TokenCookie.java
+++ src/main/java/potatoes/server/utils/crypto/TokenCookie.java
@@ -2,18 +2,22 @@ package potatoes.server.utils.crypto;
 
 import java.time.Duration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 
 public record TokenCookie(
-	String accessToken
+	String accessToken,
+	@Value("${cookie.domain}") String domain,
+	@Value("${security.jwt.token.expire-length}") String expireLength
 ) {
 	public ResponseCookie generateCookie() {
 		return ResponseCookie.from("accessToken", this.accessToken)
-			.httpOnly(true)
-			.secure(true)
+			.domain(domain)
+			.httpOnly(false) //FIXME true일시 프론트가 토큰을 못읽는다는 문제가 있는데 이야기 해볼것
+			.secure(false) // FIXME 백엔드 프론트 모두 https 도입시 none으로 변경
 			.path("/")
 			.maxAge(Duration.ofSeconds(90))
-			.sameSite("None")
+			.sameSite("Lax")    // FIXME 백엔드 프론트 모두 https 도입시 none으로 변경
 			.build();
 	}
 }
