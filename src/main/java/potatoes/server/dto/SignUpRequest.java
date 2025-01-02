f28efc23ad89a5d02d2d63f62638229be9a0efce
jickDo <pocard0117@naver.com>
2025-01-02 18:22:53 +0900

    [fix] 회원가입 밸리데이션 수정

--- src/main/java/potatoes/server/dto/SignUpRequest.java
+++ src/main/java/potatoes/server/dto/SignUpRequest.java
@@ -15,10 +15,16 @@ public record SignUpRequest(
 	)
 	String password,
 	@NotBlank(message = "이름은 필수입니다")
+	@Pattern(
+		regexp = "^[a-zA-Z가-힣]+$",
+		message = "이름은 한글과 영문만 입력 가능합니다, 공백은 불가능합니다"
+	)
 	String name,
 	@NotBlank(message = "닉네임은 필수입니다")
-	@Pattern(regexp = "^[a-zA-Z0-9가-힣]{2,10}$", message = "닉네임은 2-10자의 한글, 영문, 숫자만 가능합니다")
-	String nickname,
+	@Pattern(
+		regexp = "^[a-zA-Z0-9가-힣]([a-zA-Z0-9가-힣]|(?<!\\s)\\s(?!\\s)){0,8}[a-zA-Z0-9가-힣]$",
+		message = "닉네임은 2-10자의 한글, 영문, 숫자만 가능하며 시작과 끝은 공백이 불가능합니다. 연속된 공백은 불가능합니다"
+	) String nickname,
 	@NotBlank(message = "생일은 필수입니다")
 	@Pattern(
 		regexp = "^\\d{4}(0[1-9]|1[0-2])(0[1-9]|[12]\\d|3[01])$",
@@ -26,6 +32,10 @@ public record SignUpRequest(
 	)
 	String birthDate,
 	@NotBlank(message = "연락처는 필수입니다.")
+	@Pattern(
+		regexp = "^\\d{3}-\\d{4}-\\d{4}$",
+		message = "연락처는 '000-0000-0000' 형식으로 입력해주세요"
+	)
 	String contact
 ) {
 }
