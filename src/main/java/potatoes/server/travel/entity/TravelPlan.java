c8fa8d803e9c2d508f02e76f16172cb46a8fa147
Lim Jick Chan <95222741+jickDo@users.noreply.github.com>
2025-02-06 13:50:11 +0900

    🐛 fix : 오류케이스 검증 (#226)

--- src/main/java/potatoes/server/travel/entity/TravelPlan.java
+++ src/main/java/potatoes/server/travel/entity/TravelPlan.java
@@ -3,7 +3,6 @@ package potatoes.server.travel.entity;
 import static jakarta.persistence.FetchType.*;
 import static jakarta.persistence.GenerationType.*;
 import static lombok.AccessLevel.*;
-import static potatoes.server.utils.error.ErrorCode.*;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -15,7 +14,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import potatoes.server.travel.model.TravelPlanModel;
-import potatoes.server.utils.error.exception.WeGoException;
 
 @Getter
 @NoArgsConstructor(access = PROTECTED)
@@ -48,7 +46,6 @@ public class TravelPlan {
 	@Builder
 	public TravelPlan(Travel travel, int tripDay, int tripOrderNumber, String destination, String image,
 		String description) {
-		validateTripDay(tripDay, travel.getTripDuration());
 
 		this.travel = travel;
 		this.tripDay = tripDay;
@@ -67,11 +64,5 @@ public class TravelPlan {
 			.destination(model.destination())
 			.description(model.description())
 			.build();
-	}
-
-	private void validateTripDay(int tripDay, int tripDuration) {
-		if (tripDay > tripDuration) {
-			throw new WeGoException(INVALID_TRAVEL_DETAIL_INFO);
-		}
 	}
 }
